35b4e16e8436540dd4314994c799c2f8554233ee
AndriiGit <neo.andreu@gmail.com>
2022-09-11 17:51:26 +0300

    SH74HC595.h

---
+++ SH74HC595.h
@@ -0,0 +1,16 @@
+/*
+ * SH74HC595.h
+ *
+ *  Created on: Sep 6, 2022
+ *      Author: neo-a
+ */
+#include "main.h"
+#ifndef INC_SH74HC595_H_
+#define INC_SH74HC595_H_
+
+void Init();
+void Forward();
+void Backward();
+void Reset();
+
+#endif /* INC_SH74HC595_H_ */
